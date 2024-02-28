f40659c6a11fbac4ad34a2795d6490e0c64a6fcc
Neeti <neetijample7@gmail.com>
2024-02-28 19:29:21 +0530

    Adding functionalities to the slot machine

--- main.py
+++ main.py
@@ -16,6 +16,29 @@ symbol_count  = {
     "D": 8
 }
 
+symbol_value  = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append[line + 1]
+        
+    return winnings,winning_lines
+
+
 def get_slot_machine_spin(rows,cols,symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -86,11 +109,7 @@ def get_bet():
             print("Please enter a number.")
     return amount
 
-    
-
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -105,5 +124,21 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines= check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won Rs.{winnings}.")
+    print(f"You won on", *winning_lines) 
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is Rs.{balance}")
+        answer = input("Press enter to play(q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with Rs.{balance}")
 
 main()
